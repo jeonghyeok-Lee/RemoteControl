1bc72f803806ae7e173d897ac5cd8447017b2a84
argon <argon@Argonaut>
2022-05-28 23:31:00 +0900

    continuous connection - almost complete

--- java_remote_control/src/com/java/master/MasterJFrame.java
+++ java_remote_control/src/com/java/master/MasterJFrame.java
@@ -19,41 +19,115 @@ import javax.swing.border.LineBorder;
 import com.java.jframe.DefaultJFrame;
 import com.java.master.thread.MasterConnectThread;
 import com.java.master.thread.MasterCoummunicationThread;
+import com.java.utility.IpAddress;
 
 public class MasterJFrame extends JFrame {
-	JLabel labelIP = null, labelHost = null, labelResult = null;
+	// 연결된 슬레이브에 대한 정보를 담는 JLabel
+	JLabel labelIP = null
+			, labelHost = null
+			, labelResult = null;
+	
 	JButton btnDisConnect = null;
-	boolean connectCheck = true;
+	
+	// 마스터에 대한 정보를 담는 JLabel
+	JLabel labelMyIp = null
+			, labelMyHost = null
+			, labelMyPort = null;
+	
+	JButton btnStart = null;
+	
+	MasterConnectThread connectThread =  null;
+	
+	int port = 9095;
+	
+	ServerSocket serverSocket = null;
+	Socket socket = null;
+	
+	boolean connectCheck = false;
 	DefaultJFrame jframe = null;
-
+	
 	public MasterJFrame() {
 		setUI1();
-		settingThread();
 	}
 
 	private void setUI1() {
-		jframe = new DefaultJFrame("마스터 프로그램", 500, 300);
+		jframe = new DefaultJFrame("마스터 프로그램", 500, 600);
 
 		JPanel west = jframe.getWestPanel();
 		JPanel east = jframe.getEastPanel();
 
-		east.add(setEast());
-		east.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		east.setLayout(new GridLayout(0,1));
+		east.add(setEastController());
+		east.add(setEastNetwork());
+		east.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
 		west.add(setWest());
 		
 		jframe.addContain();
 	}
 
 	// 동쪽부분 설정
-	private JPanel setEast() {
+	private JPanel setEastController() {
 		
 		// 그룹박스 역할 수행
-		JPanel group = jframe.addGroupBox(Color.black,1,true);
+		JPanel group = jframe.addGroupBox(Color.black,2,true);
 		
 		// 실제 데이터 역할 수행
 		JPanel eastContent = new JPanel();
 
-		eastContent.setLayout(new GridLayout(3, 2));
+		eastContent.setLayout(new GridLayout(0, 2));
+		// 여백주기 시계 반대 방향
+		eastContent.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
+
+		btnStart = new JButton("ON");
+		btnStart.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if(serverSocket == null) {
+						serverSocket = new ServerSocket(port);
+						btnStart.setText("OFF");
+						connectThread = new MasterConnectThread(serverSocket, labelIP, labelHost);
+						connectThread.start();
+					}else {
+						connectThread.setStr(null);
+						System.out.println(connectThread.getStr());
+						serverSocket.close();
+						labelIP.setText("000.000.000.000");
+						labelHost.setText("미접속");
+						labelResult.setText("연결확인");
+						btnStart.setText("ON");
+					}
+				} catch (Exception e1) {
+					System.out.println(e1.getMessage());
+				}
+			}
+
+		});
+
+		IpAddress myip = new IpAddress();
+		
+		eastContent.add(new JLabel("IP  "));
+		eastContent.add(labelMyIp = new JLabel(myip.getHostAddress()));
+		eastContent.add(new JLabel("Host "));
+		eastContent.add(labelMyHost = new JLabel(myip.getHostName()));
+		eastContent.add(new JLabel("Port  "));
+		eastContent.add(labelMyPort = new JLabel(Integer.toString(port)));		
+		eastContent.add(new JLabel("Server"));
+		eastContent.add(btnStart);
+
+		group.add(eastContent);
+		return group;
+	}
+	
+	private JPanel setEastNetwork() {
+		
+		// 그룹박스 역할 수행
+		JPanel group = jframe.addGroupBox(Color.black,2,true);
+		
+		// 실제 데이터 역할 수행
+		JPanel eastContent = new JPanel();
+
+		eastContent.setLayout(new GridLayout(0, 2));
 		// 여백주기 시계 반대 방향
 		eastContent.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
@@ -61,15 +135,24 @@ public class MasterJFrame extends JFrame {
 		btnDisConnect.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				connectCheck = false;
+				if(connectThread!=null) {
+					connectThread.interrupt();
+					labelIP.setText("000.000.000.000");
+					labelHost.setText("미접속");
+					labelResult.setText("연결확인");
+					System.out.println("연결해제됨");
+				}else {
+					System.out.println("현재 연결이 되어있지 않습니다.");
+				}
+				
 			}
 
 		});
 
-		eastContent.add(new JLabel("Connect Client IP   : "));
+		eastContent.add(new JLabel("IP  "));
 		eastContent.add(labelIP = new JLabel("000.000.000.000"));
-		eastContent.add(new JLabel("Connect Client Host : "));
-		eastContent.add(labelHost = new JLabel("미접속"));
+		eastContent.add(new JLabel("Host "));
+		eastContent.add(labelHost = new JLabel("미접속"));	
 		eastContent.add(labelResult = new JLabel("연결확인"));
 		eastContent.add(btnDisConnect);
 
@@ -88,7 +171,17 @@ public class MasterJFrame extends JFrame {
 		JButton btnOption = new JButton("Option");
 		
 		westContent.setLayout(new GridLayout(0,1));
-		westContent.setBorder(BorderFactory.createEmptyBorder(10, 30, 30, 10));
+		westContent.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 10));
+		
+		btnController.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setBackground(new Color(255,128,0));
+				
+			}
+			
+		});
 		
 		westContent.add(btnController);
 		westContent.add(btnNetwork);
@@ -98,21 +191,4 @@ public class MasterJFrame extends JFrame {
 		return westContent;
 	}
 	
-	// 스레드 설정
-	private void settingThread() {
-		ServerSocket serverSocket = null;
-		Socket socket = null;
-		try {
-			serverSocket = new ServerSocket(9095);
-			socket = serverSocket.accept();
-			MasterConnectThread connectThread = new MasterConnectThread(socket, labelResult, labelIP, labelHost ,connectCheck);
-			MasterCoummunicationThread mouseThread = new MasterCoummunicationThread(socket);
-			connectThread.start();
-			mouseThread.start();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} finally {
-
-		}
-	}
 }
--- java_remote_control/src/com/java/master/thread/MasterConnectThread.java
+++ java_remote_control/src/com/java/master/thread/MasterConnectThread.java
@@ -3,46 +3,58 @@ package com.java.master.thread;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.swing.JLabel;
 
 public class MasterConnectThread extends Thread {
-	// 다중접속을 위함
-//	static List<PrintWriter> list = Collections.synchronizedList(new ArrayList<PrintWriter>());
+	private JLabel labelIP = null, labelHost = null;
+	private Socket socket = null;
+	private ServerSocket server = null;
+	private String str = null;
 
-	Socket socket = null;
-	JLabel labelResult = null, labelIP = null, labelHost = null;
-	boolean check = true;
+	public String getStr() {
+		return str;
+	}
 
-	public MasterConnectThread(Socket socket, JLabel labelResult, JLabel labelIP, JLabel labelHost) {
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public MasterConnectThread(ServerSocket server, JLabel labelIP, JLabel labelHost) {
+		this.server = server;
+		this.labelIP = labelIP;
+		this.labelHost = labelHost;
+	}
+	
+	// 서버소켓이 아닌 소켓을 가져와서 처리 가능한 방법 알아보기
+	public MasterConnectThread(Socket socket, JLabel labelIP, JLabel labelHost) {
 		this.socket = socket;
-		this.labelResult = labelResult;
 		this.labelIP = labelIP;
 		this.labelHost = labelHost;
 	}
 
-	public MasterConnectThread(Socket socket, JLabel labelResult, JLabel labelIP, JLabel labelHost, boolean check) {
-		this.socket = socket;
-		this.labelResult = labelResult;
-		this.labelIP = labelIP;
-		this.labelHost = labelHost;
-		this.check = check;
-	}
-
+	// 지속 접속
 	public void run() {
+		run2();
+	}
+	
+	public void run2() {
 		InetAddress myNet = null, inet = null;
-		String str = null;
 		DataOutputStream dataOutStream = null;
 		DataInputStream dataInStream = null;
 
 		try {
-			if (check) {
-				// 추후 제거
-				labelResult.setText("사용자가 접속하였습니다.");
-
+			myNet = InetAddress.getLocalHost();
+			// 연결해제 연결을 지속하기 위한 while문
+			while (true) {
+				// 버튼에서 처리시 버튼이 소켓이 수신을 대기하는동안 계속 눌려있으므로 여기서 처리
+				socket = server.accept();
 				// 상대에게서 받는 문자열을 읽어들이기 위한 DataInputStream객체 생성
 				dataInStream = new DataInputStream(socket.getInputStream());
+				dataOutStream = new DataOutputStream(socket.getOutputStream());
+				
 				// 상대에게서 넘어온 문자열 즉, hostName을 문자열로 저장
 				String hostName = dataInStream.readUTF();
 
@@ -50,26 +62,23 @@ public class MasterConnectThread extends Thread {
 				labelIP.setText(inet.getHostAddress().toString());
 				labelHost.setText(hostName);
 
-				myNet = InetAddress.getLocalHost();
-				str = "Connect IP : " + myNet.getHostAddress().toString() + "-ConnectHostName :"
-						+ myNet.getHostName().toString();
-				dataOutStream = new DataOutputStream(socket.getOutputStream());
+				str = "Connect IP : " + myNet.getHostAddress().toString() + "-ConnectHostName :" + myNet.getHostName().toString();
+				
 				dataOutStream.writeUTF(str); // 문자열의 형태로 값 전달
 				dataOutStream.flush();
 
-			} else {
-				labelResult.setText("연결이 해제되었습니다.");
+				System.out.println("연결됨");
 			}
-
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		} finally {
 			try {
-//				if (dataOutStream != null) dataOutStream.close();
-//				if (socket != null) socket.close();
+				if (dataOutStream != null) dataOutStream.close();
+				if (socket != null) socket.close();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
+
 }
