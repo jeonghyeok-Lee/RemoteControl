f8a5b6493bca7a6095e21e99d76ca09f4af5322a
argon <argon@Argonaut>
2022-06-02 00:12:57 +0900

    pass mouse value

--- java_remote_control/src/com/java/master/MasterJFrame.java
+++ java_remote_control/src/com/java/master/MasterJFrame.java
@@ -1,24 +1,24 @@
 package com.java.master;
 
-import java.awt.BorderLayout;
+import java.awt.AWTEvent;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.MouseInfo;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
 
 import com.java.jframe.DefaultJFrame;
 import com.java.master.thread.MasterConnectThread;
-import com.java.master.thread.MasterCoummunicationThread;
 import com.java.utility.IpAddress;
 
 public class MasterJFrame extends JFrame {
@@ -37,22 +37,27 @@ public class MasterJFrame extends JFrame {
 	private JButton btnStart = null;
 	
 	private boolean serverOnOff = false;
+
+//	private Socket socket = null;
 	
 	
+	public boolean isServerOnOff() {
+		return serverOnOff;
+	}
+
 	private ServerSocket serverSocket = null;
-	private Socket socket = null;
 	private MasterConnectThread connectThread =  null;
-	private int port = 9095;
+	private final static int PORT = 9095;
 	
 	private DefaultJFrame jframe = null;
 	
 	public MasterJFrame() {
 		setUI1();
-	
 	}
 
 	private void setUI1() {
 		jframe = new DefaultJFrame("마스터 프로그램", 500, 600);
+		jframe.setPanel();
 
 		JPanel west = jframe.getWestPanel();
 		JPanel east = jframe.getEastPanel();
@@ -64,7 +69,9 @@ public class MasterJFrame extends JFrame {
 		west.add(setWest());
 		
 		jframe.addContain();
+
 	}
+	
 
 	// 동쪽부분 설정
 	private JPanel setEastController() {
@@ -87,12 +94,17 @@ public class MasterJFrame extends JFrame {
 				try {
 					if(btnStart.getText().equals("ON")) {
 						btnStart.setText("OFF");
-						serverSocket = new ServerSocket(port);
-						connectThread = new MasterConnectThread(serverSocket, labelIP, labelHost);
+						serverSocket = new ServerSocket(PORT);
+						serverOnOff = true;
+						connectThread = new MasterConnectThread(serverSocket, labelIP, labelHost, jframe);
 						connectThread.start();
+						
 					}else {
 						if(connectThread != null) connectThread.interrupt();
-						if(connectThread != null) serverSocket.close();
+						// 현재 서버소켓이 열려있는지 확인
+						if(isOpen(serverSocket)) {
+							serverSocket.close();
+						}
 						
 						labelIP.setText("000.000.000.000");
 						labelHost.setText("미접속");
@@ -113,7 +125,7 @@ public class MasterJFrame extends JFrame {
 		eastContent.add(new JLabel("Host "));
 		eastContent.add(labelMyHost = new JLabel(myip.getHostName()));
 		eastContent.add(new JLabel("Port  "));
-		eastContent.add(labelMyPort = new JLabel(Integer.toString(port)));		
+		eastContent.add(labelMyPort = new JLabel(Integer.toString(PORT)));		
 		eastContent.add(new JLabel("Server"));
 		eastContent.add(btnStart);
 
@@ -193,5 +205,10 @@ public class MasterJFrame extends JFrame {
 		
 		return westContent;
 	}
+	
+	public static boolean isOpen(ServerSocket server) {
+		return server.isBound() && !server.isClosed();
+	}
+	
 	
 }
