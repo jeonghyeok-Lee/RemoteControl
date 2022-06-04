4acf627a428efc53d478667d7dccdc25547c606e
argon <argon@Argonaut>
2022-06-04 22:20:19 +0900

    East content change when clicked

--- java_remote_control/src/com/java/master/MasterJFrame.java
+++ java_remote_control/src/com/java/master/MasterJFrame.java
@@ -1,14 +1,10 @@
 package com.java.master;
 
-import java.awt.AWTEvent;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
-import java.awt.MouseInfo;
-import java.awt.Toolkit;
-import java.awt.event.AWTEventListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.net.ServerSocket;
 
 import javax.swing.BorderFactory;
@@ -38,9 +34,6 @@ public class MasterJFrame extends JFrame {
 	
 	private boolean serverOnOff = false;
 
-//	private Socket socket = null;
-	
-	
 	public boolean isServerOnOff() {
 		return serverOnOff;
 	}
@@ -50,29 +43,43 @@ public class MasterJFrame extends JFrame {
 	private final static int PORT = 9095;
 	
 	private DefaultJFrame jframe = null;
+	private JPanel west = null, east = null;
+	
+	private CardLayout card = null;
+	JPanel eastContent = null;
 	
 	public MasterJFrame() {
 		setUI1();
 	}
 
 	private void setUI1() {
-		jframe = new DefaultJFrame("마스터 프로그램", 500, 600);
+		jframe = new DefaultJFrame("마스터 프로그램", 500, 350);
 		jframe.setPanel();
-
-		JPanel west = jframe.getWestPanel();
-		JPanel east = jframe.getEastPanel();
-
-		east.setLayout(new GridLayout(0,1));
-		east.add(setEastController());
-		east.add(setEastNetwork());
-		east.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+		
+		east = jframe.getEastPanel();
+		west = jframe.getWestPanel();
+		
+		east.add(setEast());
 		west.add(setWest());
 		
 		jframe.addContain();
 
 	}
 	
-
+	private JPanel setEast() {
+		
+		card = new CardLayout();
+		
+		eastContent = new JPanel(card);
+		
+		eastContent.add(setEastController(), "controller");
+		eastContent.add(setEastNetwork() , "network");
+		eastContent.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 10));
+		card.show(eastContent, "controller");
+		
+		return eastContent;
+		
+	}
 	// 동쪽부분 설정
 	private JPanel setEastController() {
 		
@@ -92,6 +99,7 @@ public class MasterJFrame extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				
 				try {
+
 					if(btnStart.getText().equals("ON")) {
 						btnStart.setText("OFF");
 						serverSocket = new ServerSocket(PORT);
@@ -100,7 +108,8 @@ public class MasterJFrame extends JFrame {
 						connectThread.start();
 						
 					}else {
-						if(connectThread != null) connectThread.interrupt();
+						// 현재 스레드의 상태가 종료되어있지 않다면 종료
+						if(connectThread.getState() != Thread.State.TERMINATED) connectThread.interrupt();
 						// 현재 서버소켓이 열려있는지 확인
 						if(isOpen(serverSocket)) {
 							serverSocket.close();
@@ -111,6 +120,7 @@ public class MasterJFrame extends JFrame {
 						labelResult.setText("연결확인");
 						btnStart.setText("ON");
 					}
+
 				} catch (Exception e1) {
 					System.out.println(e1.getMessage());
 				}
@@ -143,15 +153,19 @@ public class MasterJFrame extends JFrame {
 
 		eastContent.setLayout(new GridLayout(0, 2));
 		// 여백주기 시계 반대 방향
-		eastContent.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		eastContent.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
 
 		btnDisConnect = new JButton("연결해제");
 		btnDisConnect.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(connectThread!=null) {
+			public void actionPerformed(ActionEvent e) { // 현재 실행되어있는 스레드 종료 이후 다시 실행
+				if(connectThread.getState() != Thread.State.TERMINATED) {
 					connectThread.interrupt();
 					System.out.println("connectThread 인터럽트");
+					
+					connectThread =  new MasterConnectThread(serverSocket, labelIP, labelHost, jframe);
+					connectThread.start();
+					System.out.println("connectThread 다시실행");
 					
 					labelIP.setText("000.000.000.000");
 					labelHost.setText("미접속");
@@ -192,8 +206,36 @@ public class MasterJFrame extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				setBackground(new Color(255,128,0));
-				
+				System.out.println("Controller 클릭");
+				card.show(eastContent,"controller");
+			}
+			
+		});
+		
+		btnNetwork.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Network 클릭");
+				card.show(eastContent,"network");
+			}
+			
+		});
+		
+		btnAccount.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Account 클릭");
+			}
+			
+		});
+		
+		btnOption.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Option 클릭");
 			}
 			
 		});
