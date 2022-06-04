116bf8c019055021c8898baea524bed1dfe2c144
argon <argon@Argonaut>
2022-06-04 22:24:23 +0900

    Master / Slave Program default set

--- java_remote_control/src/com/java/master/MasterProgram.java
+++ java_remote_control/src/com/java/master/MasterProgram.java
@@ -1,18 +1,13 @@
 package com.java.master;
 
 import com.java.jframe.AccountSignUp;
-import com.java.jframe.LogInJFrame;
 
 public class MasterProgram {
 
 	public static void main(String[] args) {
 //		new LogInJFrame(false);
-		new AccountSignUp();
-//		CmdConnect cmd = new CmdConnect();
-//		System.out.println(cmd.execCmd("java -version"));
-//		new MasterJFrame();
-//		new CheckLinkConsole().checkLink();
+//		new AccountSignUp(false);
+//		MasterJFrame mJFrame = new MasterJFrame();
 	}
-
 }
 
--- java_remote_control/src/com/java/slave/SlaveProgram.java
+++ java_remote_control/src/com/java/slave/SlaveProgram.java
@@ -7,7 +7,5 @@ public class SlaveProgram {
 	public static void main(String[] args) {
 //		new LogInJFrame(true);
 //		new SlaveJFrame();
-//		new LinkConsole().checkLinkConsole();
-
 	}
 }
