346424826b3a7817ac0b7fd0c5505729290d1213
jeonghyeok.lee <jeonghyeok.lee.dev@gmail.com>
2022-07-01 22:48:51 +0900

    Change of master part according to modularization

--- java_remote_control/src/com/java/master/MasterFrame.java
+++
@@ -1,5 +0,0 @@
-package com.java.master;
-
-public class MasterFrame {
-
-}
--- java_remote_control/src/com/java/master/MasterProgram.java
+++ java_remote_control/src/com/java/master/MasterProgram.java
@@ -1,15 +1,10 @@
 package com.java.master;
 
-import com.java.jframe.AccountSignUp;
-import com.java.jframe.LogInJFrame;
 
 public class MasterProgram {
 
 	public static void main(String[] args) {
-//		new LogInJFrame(false);
-		new AccountSignUp(false);
-//		new MasterJFrame("devilljh2","a123456789!");
-		
+//		new MasterMain();
 	}
 }
 
